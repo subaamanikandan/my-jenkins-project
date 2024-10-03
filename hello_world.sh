b5e8014a6c2febea59c0925987ce2867ad80412e
subaamanikandan <subaamanikandan@gmail.com>
2024-10-03 12:44:03 +0530

    Update hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -5,3 +5,7 @@ git add hello_world.sh
 git commit -m "Add hello world script"
 https://github.com/subaamanikandan/my-jenkins-project.git
 git push -u origin master
+git add .
+git commit -m "Testing Jenkins build trigger"
+git push origin master
+
