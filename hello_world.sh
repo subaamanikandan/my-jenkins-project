9ae7936e4f1339828deb74db6e80d89c867570ff
subaamanikandan <subaamanikandan@gmail.com>
2024-10-03 12:08:24 +0530

    Create hello_world.sh

---
+++ hello_world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hello, World!"
+git init
+git add hello_world.sh
+git commit -m "Add hello world script"
+https://github.com/subaamanikandan/my-jenkins-project.git
+git push -u origin master
